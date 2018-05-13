2535ebad82c6174f88bf77d9fb6d8c4333e3373c
Zach Burton <zachburton@Zachs-iMac.local>
2018-05-13 15:14:12 -0400

    Lesson 2 - Commit 1

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,24 @@
+//
+//  ChatVc.swift
+//  Smack
+//
+//  Created by Zach Burton on 5/12/18.
+//  Copyright © 2018 Zach Burton. All rights reserved.
+//
+
+import UIKit	
+
+class ChatVC: UIViewController {
+    
+    // Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+
+
+    }
+
+}
